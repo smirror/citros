b768ca09656faea02f5d80645931ce74feeddadc
smirror <23025996+smirror@users.noreply.github.com>
2025-06-27 00:28:25 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 #![no_std]
+#![no_main]
 
 use core::arch::asm;
+use core::panic::PanicInfo;
 
-#[no_mangle]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        unsafe {
+            asm!("hlt");
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
 pub extern "C" fn KernelMain() -> ! {
     loop {
         unsafe {
             asm!("hlt");
         }
     }
+}
+
+// UEFI用のエントリーポイント
+#[unsafe(no_mangle)]
+pub extern "C" fn efi_main() -> ! {
+    KernelMain()
 }
